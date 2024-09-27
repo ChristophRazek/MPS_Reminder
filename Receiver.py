dfc20d4dc2ad1b98ad4447b6f4df3d6085cf7142
ChristophRazek <52331031+ChristophRazek@users.noreply.github.com>
2024-09-27 09:16:57 +0200

    wanzi verteiler aktualisiert

--- Receiver.py
+++ Receiver.py
@@ -1,5 +1,5 @@
 
-contacts = {70053: ['jackyshen@wanziltd.com','ramonashen@wanziltd.com','daisyshen@wanziltd.com'],
+contacts = {70053: ['jackyshen@wanziltd.com','ramonashen@wanziltd.com','daisyshen@wanziltd.com','evatang@wanziltd.com'],
             70005: ['corrine.lo@beautymarkltd.com', 'kate.shih@beautymarkltd.com'],
             70047: ['carrie@alltagroup.com', 'miky.guo@alltagroup.com'],
             70065: ['f.schilling@bakic.com', 'c.weinzierl@bakic.com'],
