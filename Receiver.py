4e50f33c66f113e81945cc3b9ae21553a9cd9a0e
ChristophRazek <52331031+ChristophRazek@users.noreply.github.com>
2024-11-07 10:05:11 +0100

    empfänger Bakic auf Q- Wunsch ausgesetzt

--- Receiver.py
+++ Receiver.py
@@ -2,7 +2,7 @@
 contacts = {70053: ['jackyshen@wanziltd.com','ramonashen@wanziltd.com','daisyshen@wanziltd.com','evatang@wanziltd.com'],
             70005: ['corrine.lo@beautymarkltd.com', 'Danni.Chen@beautymarkltd.com'],
             70047: ['carrie@alltagroup.com', 'miky.guo@alltagroup.com'],
-            70065: ['f.schilling@bakic.com', 'c.weinzierl@bakic.com'],
+            #70065: ['f.schilling@bakic.com', 'c.weinzierl@bakic.com'],
             70063: ['sarah.su@yuanharng.com', 'tina.cao@yuanharng.com'],
             70085: ['monica.liu@micentech.com', 'elmer@micentech.com'],
             70072: ['bonnie@efapack.com', 'patty@efapack.com','francesca@efapack.com'],
